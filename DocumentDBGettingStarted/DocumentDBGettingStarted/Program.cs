c3f3847a7a4ea5fe9d22e7787f213d6c284974ad
Daniel Hansen <dph1108@gmail.com>
2018-03-12 10:54:57 +0100

    Added localhost and local key, need to be connected to DBemulator

--- DocumentDBGettingStarted/DocumentDBGettingStarted/Program.cs
+++ DocumentDBGettingStarted/DocumentDBGettingStarted/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
+// ADD THIS PART TO YOUR CODE
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
 
 namespace DocumentDBGettingStarted
 {
     class Program
     {
+        // ADD THIS PART TO YOUR CODE
+        private const string EndpointUrl = "https://localhost:8081";
+        private const string PrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        private DocumentClient client;
+
+        
         static void Main(string[] args)
         {
+            // Connect to the Azure Cosmos DB Emulator running locally
+            DocumentClient client = new DocumentClient(
+                new Uri("https://localhost:8081"),
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
         }
+
     }
 }
