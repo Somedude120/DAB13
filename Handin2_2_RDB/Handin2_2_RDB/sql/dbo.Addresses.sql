eed338057db374568c738e5278c928df07e4ad55
Daniel Hansen <dph1108@gmail.com>
2018-03-24 14:02:59 +0100

    Added some more

---
+++ Handin2_2_RDB/Handin2_2_RDB/sql/dbo.Addresses.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Addresses] (
+    [City] NVARCHAR (128) NOT NULL,
+    [Hanscity] NCHAR(10) NULL, 
+    CONSTRAINT [PK_dbo.Addresses] PRIMARY KEY CLUSTERED ([City] ASC)
+);
+
---
+++ Handin2_2_RDB/Handin2_2_RDB/sql/dbo.Contacts.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Contacts] (
+    [Hans]         NVARCHAR (128) NOT NULL,
+    [MiddleName]   NVARCHAR (MAX) NULL,
+    [SurName]      NVARCHAR (MAX) NULL,
+    [Email]        NVARCHAR (MAX) NULL,
+    [Address_City] NVARCHAR (128) NULL,
+    CONSTRAINT [PK_dbo.Contacts] PRIMARY KEY CLUSTERED ([Hans] ASC),
+    CONSTRAINT [FK_dbo.Contacts_dbo.Addresses_Address_City] FOREIGN KEY ([Address_City]) REFERENCES [dbo].[Addresses] ([City])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Address_City]
+    ON [dbo].[Contacts]([Address_City] ASC);
+
