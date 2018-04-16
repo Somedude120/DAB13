b150ae8db0bb7cc67b3fa1c203f428a1f7523d5c
Daniel Hansen <dph1108@gmail.com>
2018-04-16 17:17:01 +0200

    Følg punkt 4. Første del

--- BookService/BookService/Models/BookServiceContext.cs
+++ BookService/BookService/Models/BookServiceContext.cs
@@ -14,9 +14,11 @@ namespace BookService.Models
         // automatically whenever you change your model schema, please use data migrations.
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
+
         public BookServiceContext() : base("name=BookServiceContext")
         {
+            // New code:
+            this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
         }
 
         public System.Data.Entity.DbSet<BookService.Models.Author> Authors { get; set; }
